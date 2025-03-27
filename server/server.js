a23d487e8f52fbbe802e71250cc1a8fb48e6feb0
dfermic2 <dfermic2@etf.unsa.ba>
2025-03-27 16:20:46 +0100

    Connected to MongoDB

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
+require("dotenv").config();
+
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
-
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
 
 app.get("/", (req, res) => {
   res.json({ message: "Connected frontend and backend!" });
 });
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("Server running on port ", process.env.PORT);
+    });
+  })
+  .catch((error) => console.log("Error connecting to database ", error));
